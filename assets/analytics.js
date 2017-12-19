2c6ac8bafb94c186aed0cbc61e0f5cd35c5de0b3
Matthias Endler <matthias-endler@gmx.net>
2017-12-19 23:32:13 +0100

    New deploy!

--- assets/analytics.js
+++ assets/analytics.js
@@ -1,6 +1,1 @@
-<script>
-    window.ga=window.ga||function(){(ga.q = ga.q || []).push(arguments)};ga.l=+new Date;
-ga('create', 'UA-693428-1', 'auto');
-ga('send', 'pageview');
-</script>
-    <script async src='https://www.google-analytics.com/analytics.js'></script>
+<script>window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;ga('create','UA-693428-1','auto');ga('send','pageview');</script><script async src='https://www.google-analytics.com/analytics.js'></script>
